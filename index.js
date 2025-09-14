1e1f0b2b502544579bfe140bada2ad4125d93d98
Crufus <chigozirimrufus@gmail.com>
2025-09-14 04:46:35 +0100

    Initial project setup

---
+++ index.js
@@ -0,0 +1,17 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req,res) => {
+    res.render("index");
+})
+
+app.listen(port, () => {
+    console.log(`Server starts at http:localhost:${port}`);
+})
