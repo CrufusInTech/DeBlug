3256652e0b18b0a4523715a35872b4d668af46ee
Crufus <chigozirimrufus@gmail.com>
2025-09-15 23:00:38 +0100

    Completed post creation and storage logic

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Create Post Variable Array
+const createPost = [];
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -11,10 +14,22 @@ app.set("view engine", "ejs");
 app.get("/", (req,res) =>{
     res.render("post-form");
 })
-
-app.post("/home",(req,res) =>{
-    res.render("index");
+app.get("/home", (req,res) =>{
+    res.render("index",{posts: createPost});
 })
+
+app.post("/posts",(req,res) =>{
+    // Accessing my Post form details
+    const postForm = req.body;
+    console.log("recieved form data", postForm);
+
+    // Sent my post form details to my create post array
+    createPost.push(postForm);
+    console.log("This is my updated creatPost array",createPost);
+    res.redirect("/home");
+    
+  
+});
 
 app.listen(port, () => {
     console.log(`Server starts at http:localhost:${port}`);
