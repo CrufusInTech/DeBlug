3a392e5d5bbe6cf3acd335c8746c2d9d467247d3
Crufus <chigozirimrufus@gmail.com>
2025-09-22 17:02:43 +0100

    updated the menu js

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $('.menu-toggle').on('click', function() {
         $('.nav').toggleClass('active');
         $('.menu-toggle').toggleClass('bg');
+        $('.user-drop').toggleClass('user-close');
 
     });
 });
