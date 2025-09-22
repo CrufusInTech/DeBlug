7bfcb030fa45e7c59f4f0ddee197ba7f9968c642
Crufus <chigozirimrufus@gmail.com>
2025-09-22 16:50:06 +0100

    updated the header code and entire ui

--- public/main.js
+++ public/main.js
@@ -2,5 +2,7 @@
 $(document).ready(function() {
     $('.menu-toggle').on('click', function() {
         $('.nav').toggleClass('active');
+        $('.menu-toggle').toggleClass('bg');
+
     });
 });
