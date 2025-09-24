5999e424785ffbb01d6e9b8d189400962a53c52d
Crufus <chigozirimrufus@gmail.com>
2025-09-24 18:08:20 +0100

    Carousel work ongoing too

--- public/main.js
+++ public/main.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
     $('.menu-toggle').on('click', function() {
         $('.nav').toggleClass('active');
-        $('.menu-toggle').toggleClass('bg');
 
     });
 });
