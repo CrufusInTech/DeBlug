ee6c425608dbc5b4f17b00ab41d9e4dd0194ffda
Crufus <chigozirimrufus@gmail.com>
2025-09-24 23:09:28 +0100

    working on carousel

--- public/main.js
+++ public/main.js
@@ -1,16 +1,15 @@
 // Hamburger functionality 
 $(document).ready(function() {
+    // Mobile menu toggle
     $('.menu-toggle').on('click', function() {
         $('.nav').toggleClass('active');
-
+        $('.menu-toggle').toggleClass('bg');
     });
-});
 
-$(document).ready(function() {
+    // User dropdown toggle, only for desktop
     $('.user-list').on('click', function() {
-        $('.drop-down').toggleClass('open');
-        $('.fa-chevron-up').toggleClass('down');
-      
+            $('.drop-down').toggleClass('open');
+            $('.fa-chevron-up').toggleClass('down');
     });
 });
 
@@ -22,5 +21,34 @@ $('.post-wrapper').slick({
   autoplaySpeed: 2000,
   nextArrow: $('.next'),
   prevArrow: $('.prev'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
 
 });
